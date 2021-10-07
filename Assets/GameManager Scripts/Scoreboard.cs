aacea4847ae79431e43e39d8a6918daa3f94e995
Acedaan <eliasdanho9@gmail.com>
2021-10-07 19:43:52 +0200

    Modified ScoreBoard

--- Assets/GameManager Scripts/Scoreboard.cs
+++ Assets/GameManager Scripts/Scoreboard.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Scoreboard : MonoBehaviour
 {
     float scoretimer;
+    public Text score;
 
     // Start is called before the first frame update
     void Start()
     {
         scoretimer = 0;
-            
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        score.text = scoretimer.ToString("F1") + "s";
+
         scoretimer += Time.deltaTime;
         Debug.Log(scoretimer);
     }

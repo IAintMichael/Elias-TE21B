42d63d6dc09b40ffeb52013a0340316a019e6727
Acedaan <eliasdanho9@gmail.com>
2021-10-07 19:17:37 +0200

    Modified Scoreboard

--- Assets/GameManager Scripts/Scoreboard.cs
+++ Assets/GameManager Scripts/Scoreboard.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Scoreboard : MonoBehaviour
 {
+    float scoretimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoretimer = 0;
+            
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        scoretimer += Time.deltaTime;
+        Debug.Log(scoretimer);
     }
 }
